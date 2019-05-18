43a4936cb5b43afd6a43027b4058ae92e5b5ed26
David Cook <divergentdave@gmail.com>
2019-05-18 17:12:34 -0500

    Add test case for URL formatting

---
+++ test/test_format_coords.py
@@ -0,0 +1,17 @@
+import unittest
+
+from registrars.query import format_url
+
+
+class CoordinateTestCase(unittest.TestCase):
+    def test_hennepin(self):
+        registrar_dict = {
+            "url_format": ("https://gis.hennepin.us/Property/map/default.aspx?"
+                           "C={coord1},{coord2}&L=7"),
+            "coordinate_epsg": 26915,
+        }
+        gps_location = (-93.3, 45.0)
+        url = format_url(registrar_dict, gps_location)
+        self.assertEqual(url,
+                         "https://gis.hennepin.us/Property/map/default.aspx?"
+                         "C=476355.4109102419,4982994.171200306&L=7")
