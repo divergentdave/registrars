516534d7719b62699f332493500201649183579e
David Cook <divergentdave@gmail.com>
2019-05-18 14:02:42 -0500

    Rename key

--- registrars/__init__.py
+++ registrars/__init__.py
@@ -13,8 +13,9 @@ def format_url(registrar_dict, gps_location):
     url_format = registrar_dict.get("url_format")
     if url_format is None:
         return None
-    if "epsg" in registrar_dict:
-        epsg = int(registrar_dict["epsg"])
+    epsg = registrar_dict.get("coordinate_epsg")
+    if epsg is not None:
+        epsg = int(epsg)
         proj = pyproj.Proj(init="epsg:{}".format(epsg))
         x, y = pyproj.transform(PROJ_WGS84, proj, *gps_location)
         return url_format.format(coord1=x, coord2=y)
