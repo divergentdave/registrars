c7a8133ddacbafa9e2e223d40f176e40bb35c87a
David Cook <divergentdave@gmail.com>
2019-05-19 14:55:32 -0500

    Visualize coverage of registrar data

---
+++ visualize.py
@@ -0,0 +1,18 @@
+import osmnx
+import yaml
+
+osmnx.settings.use_cache = True
+
+
+def main():
+    with open("data.yaml") as f:
+        data = yaml.safe_load(f)
+
+    names = [registrar_dict["osm_name"] for registrar_dict in data]
+    gdf = osmnx.gdf_from_places(names)
+    print(gdf)
+    osmnx.plot_shape(gdf)
+
+
+if __name__ == "__main__":
+    main()
