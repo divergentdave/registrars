2d499577846a15a16656f0a872df01149a70fcdc
David Cook <divergentdave@gmail.com>
2019-05-20 22:11:54 -0500

    Add logic to Lambda handler

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -13,11 +13,22 @@ def initialize():
 
 def lambda_handler(event, context):
     global index
+    body = json.loads(event["body"])
+    gps_location = (body["longitude"], body["latitude"])
     payload = [
+        {
+            "osm_name": registrar_dict["osm_name"],
+            "url": registrars.query.format_url(registrar_dict, gps_location),
+        }
+        for registrar_dict in registrars.query.search_index(gps_location,
+                                                            index)
     ]
     return {
         "statusCode": "200",
-        "body": json.dumps(payload),
+        "body": json.dumps(payload) + "\n",
+        "headers": {
+            "Content-Type": "application/json",
+        },
     }
 
 
