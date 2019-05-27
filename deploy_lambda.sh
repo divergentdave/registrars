1d5d0935146fceb8dd9bac973fc8f490ad80445f
David Cook <divergentdave@gmail.com>
2019-05-27 14:33:00 -0500

    Prevent deploying development config to Lambda

--- deploy_lambda.sh
+++ deploy_lambda.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+if [ ! -f .aws-sam/build/template.yaml ]; then
+    echo "Run build_lambda_prod.sh before deploying" 1>&2
+    exit 1
+fi
+if grep -q "localhost\|127.0.0.1" .aws-sam/build/template.yaml; then
+    echo "Run build_lambda_prod.sh before deploying" 1>&2
+    exit 1
+fi
 set -e
 sam package --output-template-file packaged.yaml --s3-bucket divergentdave-registrars
 sam deploy --template-file packaged.yaml --stack-name registrars --capabilities CAPABILITY_IAM
