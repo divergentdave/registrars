120bfd4137da64ebb651933826a6e1716630034d
David Cook <divergentdave@gmail.com>
2019-08-18 17:53:06 -0500

    Fix typo

--- sitecustomize.py
+++ sitecustomize.py
@@ -1,5 +1,5 @@
 from aws_lambda_builders.workflows.python_pip.workflow import PythonPipWorkflow
-PythonPipWorkflow.EXDLUDED_FILES = tuple(
+PythonPipWorkflow.EXCLUDED_FILES = tuple(
     pat for pat in PythonPipWorkflow.EXCLUDED_FILES
     if pat != "*.so"
 )
