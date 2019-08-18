0907e614a9d438279a8284a4c25bfb4bbdbdff8e
David Cook <divergentdave@gmail.com>
2019-08-18 17:25:15 -0500

    Change workaround used for sam build
    https://github.com/awslabs/aws-lambda-builders/issues/115#issuecomment-521612943

--- build_lambda_dev.sh
+++ build_lambda_dev.sh
@@ -4,7 +4,4 @@ set -e
 (cd lambda && python -m registrars.build)
 docker build -t libspatialindex --file Dockerfile.libspatialindex .
 docker run -v "$(pwd)/lambda/lib:/target" libspatialindex
-sam build --template template-dev.yaml
-(cd .aws-sam/build/RegistrarsAppFunction/lib/ &&
-    ln -s libspatialindex.so.4.0.1 libspatialindex.so &&
-    ln -s libspatialindex_c.so.4.0.1 libspatialindex_c.so)
+PYTHONPATH=. sam build --template template-dev.yaml
