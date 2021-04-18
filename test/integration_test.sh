b102e132895fc0a779d3fcfb33e12c1408577781
David Cook <divergentdave@gmail.com>
2021-04-18 13:31:04 -0500

    Fix integration test

--- test/integration_test.sh
+++ test/integration_test.sh
@@ -3,5 +3,5 @@ set -e
 request=$(sam local generate-event apigateway aws-proxy --body '{"longitude":-93.3,"latitude":45.0}' --stage prod --method POST --path / --account-id 300689510484 --dns-suffix ca-central-1.amazonaws.com)
 response=$(echo $request | sam local invoke --event -)
 echo $response
-echo $response | grep -q '"statusCode":"200"'
-echo $response | grep -q '\\"https://gis.hennepin.us/property/map/default.aspx?C=476355.4109102419,4982994.171200306\\u0026L=7\\"'
+echo $response | grep -q '"statusCode": *"200"'
+echo $response | grep -q '\\"https://gis\.hennepin\.us/property/map/default\.aspx?C=476355\.4109102419,4982994\.171200306\(\\u0026\|&\)L=7\\"'
